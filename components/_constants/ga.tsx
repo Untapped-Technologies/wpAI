ab45c04500a17a029bc7a85ef5493a95c273f9dc
ezequielcabrera <ezcabrera@gmail.com>
2025-10-03 06:39:26 -0500

    updated script

--- components/_constants/ga.tsx
+++ components/_constants/ga.tsx
@@ -1,13 +1,15 @@
+import Script from 'next/script'
+
 const GA = () => {
   return (
     <>
       {process.env.NEXT_PUBLIC_GA_ID && (
         <>
-          <script
+          <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
             strategy="afterInteractive"
           />
-          <script id="ga-script" strategy="afterInteractive">
+          <Script id="ga-script" strategy="afterInteractive">
             {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
@@ -16,7 +18,7 @@ const GA = () => {
                   page_path: window.location.pathname,
                 });
               `}
-          </script>
+          </Script>
         </>
       )}
     </>
